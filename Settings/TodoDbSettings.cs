8132e57ae339884e9e49ce8ae923cd61c9f25331
JoshBenPhilip <joshben4c@gmail.com>
2022-06-08 17:48:00 -0400

    built api and backend and connnected to DB with errors

---
+++ Settings/TodoDbSettings.cs
@@ -0,0 +1,9 @@
+namespace Settings;
+
+public class TodoDbSettings
+{
+
+    public string ConnectionString {get; set;}
+    public string DatabaseName {get; set;}
+    public string CollectionName {get; set;}
+}
